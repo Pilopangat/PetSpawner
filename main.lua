615e1710a1a94d75b5d042c97e2b3385d980f010
Pilopangat <yoshionly23@Gmail.com>
2025-06-23 14:18:38 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
 local scripts = {
-    [126884695634066] = "https://github.com/Pilopangat/PetSpawner/blob/main/bdf03980d3e108701305d0301106ba24.txt",
-    [81440632616906] = "https://github.com/Pilopangat/PetSpawner/blob/main/bdf03980d3e108701305d0301106ba24.txt",
+    [126884695634066] = "https://raw.githubusercontent.com/Pilopangat/PetSpawner/refs/heads/main/af7c6ba391c435458bfbc93f60a4372b.txt",
+    [81440632616906] = "https://raw.githubusercontent.com/Pilopangat/PetSpawner/refs/heads/main/af7c6ba391c435458bfbc93f60a4372b.txt",
 }
 
 local url = scripts[game.PlaceId]
 if url then
     loadstring(game:HttpGetAsync(url))()
-    loadstring(game:HttpGetAsync("https://github.com/Pilopangat/PetSpawner/blob/main/bdf03980d3e108701305d0301106ba24.txt"))()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Pilopangat/PetSpawner/refs/heads/main/af7c6ba391c435458bfbc93f60a4372b.txt))()
 end
